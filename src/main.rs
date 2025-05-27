2ea8c8813e4475de80392d2cd9f6a6f4c1506a24
George Stavropoulos <georgestavropoulos0@gmail.com>
2025-05-27 08:49:41 +0100

    trying out backtrace frames

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ mod objects;
 use authenticate as auth;
 
 use std::{
+    env,
+    backtrace::Backtrace,
     collections::{VecDeque, HashMap},
     time::{Duration, SystemTime},
     fs::File,
@@ -23,22 +25,30 @@ use reqwest::{
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let token = auth::token().await?;
+    // let token = auth::token().await?;
 
-    let mut headers = HeaderMap::new();
-    headers.insert(AUTHORIZATION, format!("Bearer {token}").parse().unwrap());
-    headers.insert(CONTENT_TYPE, "application/json".parse().unwrap());
+    // let mut headers = HeaderMap::new();
+    // headers.insert(AUTHORIZATION, format!("Bearer {token}").parse().unwrap());
+    // headers.insert(CONTENT_TYPE, "application/json".parse().unwrap());
 
-    let client = Client::builder()
-        .connection_verbose(true)
-        .default_headers(headers)
-        .build()?;
+    // let client = Client::builder()
+    //     .connection_verbose(true)
+    //     .default_headers(headers)
+    //     .build()?;
 
     // let pl_json = objects::pl_json(Path::new("/home/george/BULK/spotiver/playlists.json")).await?;
-    objects::backup(&client, Path::new("/home/george/BULK/spotiver"), false, false).await?;
+    // objects::backup(&client, Path::new("/home/george/BULK/spotiver"), false, false).await?;
     // let playlist_id = "7ojBoCyhFa615na068v1PB";
     // let playlist_id = "7KFoK4LJ23EncELJwYmTDG";
     // let _t = objects::tracks(&client, playlist_id).await;
+
+
+    env::set_var("RUST_BACKTRACE", "full");
+    
+    let bt = Backtrace::capture();
+    for frame in bt.frames() {
+	println!("{:?}", frame);
+    }
 
     Ok(())
 }
